fb516934307c6474b101102796ac2a4ec694eb8c
Julius Gesang <julius@familie-gesang.de>
2025-11-12 13:26:01 +0100

    style: Add license headers and apply code formatting
    - Add BSD 3-Clause license headers to all source files.
    - Apply consistent code formatting, including making composable previews private.
    - Pass down modifiers to composable functions for better reusability and layout control.

--- app/src/main/java/net/ottercloud/sliderschrank/MainActivity.kt
+++ app/src/main/java/net/ottercloud/sliderschrank/MainActivity.kt
@@ -1,3 +1,30 @@
+/*
+ * Copyright (c) 2025 OtterCloud
+ *
+ * Redistribution and use in source and binary forms, with or without * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of the copyright holder nor the names of its
+ *    contributors may be used to endorse or promote products derived from
+ *    this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 package net.ottercloud.sliderschrank
 
 import android.os.Bundle
@@ -37,7 +64,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             SliderSchrankTheme {
-                SliderSchrankApp()
+                SliderSchrankApp(modifier = Modifier.fillMaxSize())
             }
         }
     }
@@ -46,10 +73,11 @@ class MainActivity : ComponentActivity() {
 @OptIn(ExperimentalMaterial3Api::class)
 @PreviewScreenSizes
 @Composable
-fun SliderSchrankApp() {
+private fun SliderSchrankApp(modifier: Modifier = Modifier) {
     var currentDestination by rememberSaveable { mutableStateOf(AppDestinations.HOME) }
 
     NavigationSuiteScaffold(
+        modifier = modifier,
         navigationSuiteItems = {
             AppDestinations.entries.forEach { destination ->
                 item(
@@ -96,11 +124,8 @@ fun SliderSchrankApp() {
     }
 }
 
-enum class AppDestinations(
-    val label: String,
-    val icon: ImageVector,
-) {
+enum class AppDestinations(val label: String, val icon: ImageVector) {
     HOME("Home", Icons.Default.Home),
     KAMERA("Kamera", Icons.Filled.CameraEnhance),
-    KLEIDERSCHRANK("Kleiderschrank", Icons.Filled.Checkroom),
+    KLEIDERSCHRANK("Kleiderschrank", Icons.Filled.Checkroom)
 }
