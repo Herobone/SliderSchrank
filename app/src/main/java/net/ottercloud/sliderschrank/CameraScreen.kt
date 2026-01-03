4ea22d3f793fd83dc51adf009a81fc7e1c99906a
Noah <185094834+noer05@users.noreply.github.com>
2026-01-03 19:39:27 +0100

    chore: outsource button texts

--- app/src/main/java/net/ottercloud/sliderschrank/CameraScreen.kt
+++ app/src/main/java/net/ottercloud/sliderschrank/CameraScreen.kt
@@ -41,6 +41,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -81,7 +82,7 @@ fun CameraScreen(modifier: Modifier = Modifier) {
                     .fillMaxWidth()
                     .height(56.dp)
             ) {
-                Text("Foto Machen")
+                Text(stringResource(R.string.take_picture))
             }
 
             Spacer(modifier = Modifier.height(16.dp))
@@ -95,7 +96,7 @@ fun CameraScreen(modifier: Modifier = Modifier) {
                     .fillMaxWidth()
                     .height(56.dp)
             ) {
-                Text("Aus Galerie Importieren")
+                Text(stringResource(R.string.import_from_gallery))
             }
         }
     }
