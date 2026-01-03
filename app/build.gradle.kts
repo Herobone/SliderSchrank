f52c365249cf3f20dac7843e0b50694ae3cfdf4f
Julius Gesang <julius@familie-gesang.de>
2026-01-03 17:35:25 +0100

    fix: update copyright notice formatting and upgrade datastore preferences version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -60,7 +60,7 @@ dependencies {
     implementation(libs.androidx.compose.material.icons.extended)
     implementation(libs.androidx.compose.material3.adaptive.navigation.suite)
     implementation(libs.androidx.navigation.compose)
-    implementation("androidx.datastore:datastore-preferences:1.1.1")
+    implementation(libs.androidx.datastore.preferences)
 
     // Room
     implementation(libs.room.runtime)
