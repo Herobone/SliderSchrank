7864d8da8351bcc4b5b75e6ded0571bb7af7cd67
Julius Gesang <julius@familie-gesang.de>
2026-01-03 17:00:21 +0100

    fix: format copyright notice for consistency across files

--- app/src/main/java/net/ottercloud/sliderschrank/data/model/AbstractClothing.kt
+++ app/src/main/java/net/ottercloud/sliderschrank/data/model/AbstractClothing.kt
@@ -1,7 +1,8 @@
 /*
  * Copyright (c) 2025 OtterCloud
  *
- * Redistribution and use in source and binary forms, with or without * modification, are permitted provided that the following conditions are met:
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
  *
  * 1. Redistributions of source code must retain the above copyright notice, this
  *    list of conditions and the following disclaimer.
@@ -33,7 +34,7 @@ import java.util.Date
 
 abstract class AbstractClothing(
     @PrimaryKey(autoGenerate = true)
-    val id: Long = 0,
+    open val id: Long = 0,
 
     @ColumnInfo(name = "image_url")
     val imageUrl: String,
